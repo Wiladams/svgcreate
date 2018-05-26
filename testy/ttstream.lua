dd193a76ac360b71b95d68748d086e0b851555a3
William Adams <github@nanotechstyles.com>
2018-05-26 10:31:18 -0700

    adding commentary and new getters to ttstream

--- testy/ttstream.lua
+++ testy/ttstream.lua
@@ -51,12 +51,8 @@ function tt_memstream.new(self, data, size, position)
     return self:init(data, size, position);
 end
 
-
- 
-
- 
  -- move to a particular position, in bytes
- function tt_memstream.seek(self, pos)
+function tt_memstream.seek(self, pos)
     assert(not (pos > self.size or pos < 0));
     --b.cursor = (o > b.size or o < 0) ? b.size : o;
      if (pos > self.size or pos < 0) then 
@@ -66,16 +62,19 @@ end
      end
  
      return true;
- end
- 
+end
+
+-- Report the current cursor position.
+-- Good for finding out the size of something
 function tt_memstream.tell(self)
     return self.cursor;
 end
 
 
-
- function tt_memstream.skip(self, pos)
-     return self:seek(self.cursor + pos);
+-- move the cursor ahead by the amount
+-- specified in the offset
+ function tt_memstream.skip(self, offset)
+     return self:seek(self.cursor + offset);
  end
  
 -- get 8 bits, and don't advance the cursor
@@ -87,10 +86,24 @@ function tt_memstream.peek8(self)
     return self.data[self.cursor];
 end
 
- -- get as many bytes specifiec in n as an integer
+
+
+-- get 8 bits, and advance the cursor
+function tt_memstream.get8(self)
+    if (self.cursor >= self.size) then
+       return false;
+    end
+    --b.data[b.cursor++];
+    local r = self.data[self.cursor];
+    self.cursor = self.cursor + 1;
+    
+    return r
+ end
+ 
+  -- get as many bytes specifiec in n as an integer
  -- this could possibly work up to 7 byte integers
  -- converts from big endian to native format while it goes
- function tt_memstream.get(self, n)
+function tt_memstream.get(self, n)
     --STBTT_assert(n >= 1 and n <= 4);
     local v = 0;
     local i = 0;
@@ -102,23 +115,15 @@ end
     return v;
 end
 
--- get 8 bits, and advance the cursor
-function tt_memstream.get8(self)
-
-    if (self.cursor >= self.size) then
-       return false;
-    end
-    --b.data[b.cursor++];
-    local r = self.data[self.cursor];
-    self.cursor = self.cursor + 1;
-    
-    return r
- end
- 
- function tt_memstream.get16(self)  
+function tt_memstream.get16(self)  
      return self:get(2)
- end
+end
 
+function tt_memstream.get32(self)  
+    return tonumber(self:get(4))
+end
+
+-- These ensure the sign is dealth with properly
 function tt_memstream.getInt16(self)
     return tonumber(ffi.cast('int16_t', self:get(2)))
 end
@@ -127,9 +132,7 @@ function tt_memstream.getUInt16(self)
     return tonumber(ffi.cast('uint16_t', self:get(2)))
 end
 
- function tt_memstream.get32(self)  
-     return tonumber(self:get(4))
- end
+
 
 function tt_memstream.getInt32(self)
     return tonumber(ffi.cast('int32_t', self:get(4)))
@@ -138,20 +141,24 @@ end
 function tt_memstream.getUInt32(self)
     return tonumber(ffi.cast('uint32_t', self:get(4)))
 end
- -- get a subrange of the memory stream
- -- returning a new memory stream
- function tt_memstream.range(self, pos, s)
- 
-     local r = stbtt__new_buf(nil, 0);
-     if ((pos < 0) or (s < 0) or (pos > self.size) or (s > (self.size - o))) then 
-         return r;
-     end
- 
- 
-     r.data = self.data + pos;
-     r.size = s;
-     
-     return r;
- end
 
- return tt_memstream
+
+-- get a subrange of the memory stream
+-- returning a new memory stream
+function tt_memstream.range(self, pos, s)
+    if ((pos < 0) or (s < 0) or (pos > self.size) or (s > (self.size - o))) then 
+        return nil;
+    end
+
+    return tt_memstream(self.data+pos, pos, s)
+end
+
+-- Convenient types named in the documentation
+tt_memstream.getFixed = tt_memstream.getUInt32;
+tt_memstream.getFWord = tt_memstream.getInt16;
+tt_memstream.getUFWord = tt_memstream.getUInt16;
+tt_memstream.getF2Dot14 = tt_memstream.getInt16;
+tt_memstream.getOffset16 = tt_memstream.getUInt16;
+tt_memstream.getOffset32 = tt_memstream.getUInt32;
+
+return tt_memstream
